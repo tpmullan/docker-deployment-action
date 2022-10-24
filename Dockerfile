2e985efb567e0d4aa59cce5c54143ebb80a54777
Tom Mullaney <2078150+tpmullan@users.noreply.github.com>
2022-10-24 00:10:44 +0000

    Update docker compose connection style

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,18 @@
 FROM docker:stable
 
 LABEL 'name'='Docker Deployment Action'
-LABEL 'maintainer'='Al-waleed Shihadeh <wshihadeh.dev@gmail.com>'
+LABEL 'maintainer'='Thomas Mullaney <tpmullan@gmail.com>'
 
 LABEL 'com.github.actions.name'='Docker Deployment'
 LABEL 'com.github.actions.description'='supports docker-compose and Docker Swarm deployments'
 LABEL 'com.github.actions.icon'='send'
 LABEL 'com.github.actions.color'='green'
 
-RUN apk --no-cache add openssh-client docker-compose
+RUN apk --no-cache add curl openssh-client docker-compose
+
+RUN mkdir -p /usr/local/lib/docker/cli-plugins/ && \
+    curl -SL https://github.com/docker/compose/releases/download/v2.12.1/docker-compose-linux-x86_64 -o /usr/local/lib/docker/cli-plugins/docker-compose && \
+    chmod +x /usr/local/lib/docker/cli-plugins/docker-compose
 
 COPY docker-entrypoint.sh /docker-entrypoint.sh
 
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -54,7 +54,7 @@ DEPLOYMENT_COMMAND_OPTIONS=""
 if [ "$INPUT_COPY_STACK_FILE" == "true" ]; then
   STACK_FILE="$INPUT_DEPLOY_PATH/$STACK_FILE"
 else
-  DEPLOYMENT_COMMAND_OPTIONS=" --log-level debug --host ssh://$INPUT_REMOTE_DOCKER_HOST:$INPUT_REMOTE_DOCKER_PORT"
+  DEPLOYMENT_COMMAND_OPTIONS="--log-level debug --host ssh://$INPUT_REMOTE_DOCKER_HOST:$INPUT_REMOTE_DOCKER_PORT"
 fi
 
 case $INPUT_DEPLOYMENT_MODE in
@@ -65,7 +65,7 @@ case $INPUT_DEPLOYMENT_MODE in
 
   *)
     INPUT_DEPLOYMENT_MODE="docker-compose"
-    DEPLOYMENT_COMMAND="docker-compose $DEPLOYMENT_COMMAND_OPTIONS -f $STACK_FILE"
+    DEPLOYMENT_COMMAND="docker $DEPLOYMENT_COMMAND_OPTIONS compose -f $STACK_FILE"
   ;;
 esac
 
@@ -85,7 +85,7 @@ echo "Add known hosts"
 printf '%s %s\n' "$SSH_HOST" "$INPUT_SSH_PUBLIC_KEY" > /etc/ssh/ssh_known_hosts
 
 if ! [ -z "$INPUT_DOCKER_PRUNE" ] && [ $INPUT_DOCKER_PRUNE = 'true' ] ; then
-  yes | docker --log-level debug --host "ssh://$INPUT_REMOTE_DOCKER_HOST:$INPUT_REMOTE_DOCKER_PORT" system prune -a 2>&1
+  yes | docker --host "ssh://$INPUT_REMOTE_DOCKER_HOST:$INPUT_REMOTE_DOCKER_PORT" system prune -a 2>&1
 fi
 
 if ! [ -z "$INPUT_COPY_STACK_FILE" ] && [ $INPUT_COPY_STACK_FILE = 'true' ] ; then
